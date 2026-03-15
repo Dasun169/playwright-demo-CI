a151931369bd79738d467d45dcef00ba86bfca0b
Dasun169 <dasunnavindu2001@gmail.com>
2026-03-15 19:32:13 +0530

    Add user data file for form validation

---
+++ src/utils/test_data/userData.ts
@@ -0,0 +1,14 @@
+export const userData = {
+    validUserForCheckout: {
+      firstName: 'John',
+      lastName: 'Doe',
+      postalCode: '123456'
+    },
+    invalidUserForCheckout: {
+      firstName: 'John@123',
+      lastName: 'Doe@123',
+      postalCode: '123456'
+    }
+};
+  
+  
