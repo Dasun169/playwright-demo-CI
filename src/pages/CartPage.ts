871b10d2eb21e2c5433ae2758cc8a790ec9f5433
Dasun169 <dasunnavindu2001@gmail.com>
2026-03-16 14:39:18 +0530

    Enhance CartPage class with additional locators and methods for item management, including validation for continue shopping button and item removal functionality.

--- src/pages/CartPage.ts
+++ src/pages/CartPage.ts
@@ -11,17 +11,35 @@ export class CartPage {
 
     //Locators
     readonly cartPageValidationLocator = () => this.page.locator(`span.title`);
+    readonly cartItemCatchLocator = () => this.page.locator(`//div[@class="cart_item"]`);
     readonly itemNameLocator = () => this.page.locator(`div.inventory_item_name`);
     readonly itemPriceLocator = () => this.page.locator(`div.inventory_item_price`);
-    readonly itemRemoveButtonLocator = () => this.page.locator(`button.btn.btn_secondary.btn_small.cart_button`);
+    readonly itemRemoveButtonLocator = () => this.page.locator(`//button[@data-test="remove-sauce-labs-backpack"]`);
     readonly checkoutButtonLocator = () => this.page.locator(`button#checkout`);
+    readonly continueShoppingButtonLocator = () => this.page.locator(`//button[@data-test="continue-shopping"]`);
 
     //Methods
+    /**
+     * Validate the cart page
+     * by checking if the cart page validation locator is visible.
+     */
     async validateCartPage() {
         await expect(this.cartPageValidationLocator()).toHaveText('Your Cart');
         await expect(this.page).toHaveURL(process.env.BASE_URL + "cart.html");
     }
 
+    /**
+     * Open the cart page directly
+     * by navigating to the cart page URL.
+     */
+    async openCartPageDirectly() {
+        await this.page.goto(this.url);
+    }
+
+    /**
+     * Validate the item details
+     * by checking if the item name and item price locators are visible.
+     */
     async validateItemDetails(itemName: string, itemPrice: string) {
         try {
             await expect(this.itemNameLocator()).toHaveText(itemName);
@@ -32,7 +50,30 @@ export class CartPage {
         }
     }
 
+    /**
+     * by clicking the checkout button locator.
+     */
     async clickOnCheckoutButton() {
         await this.checkoutButtonLocator().click();
+    }
+
+    /**
+     * Validate the continue shopping button
+     * by checking if the continue shopping button locator is visible.
+     */
+    async continueShoppingButtonValidation() {
+        await expect(this.continueShoppingButtonLocator()).toBeVisible();
+        await this.continueShoppingButtonLocator().click();
+        await expect(this.page).toHaveURL(process.env.BASE_URL + "inventory.html");
+    }
+
+    /**
+     * Remove item from cart
+     * by clicking the remove button locator.
+     */
+    async removeItemFromCart() {
+        let count = await this.cartItemCatchLocator().count();
+        await this.itemRemoveButtonLocator().click();
+        await expect(this.cartItemCatchLocator()).toHaveCount(count - 1);
     }
 }
