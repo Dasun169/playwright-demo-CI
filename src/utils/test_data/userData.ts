10214d441033962138a97825512a34b74ec7e133
Dasun169 <dasunnavindu2001@gmail.com>
2026-03-16 14:40:49 +0530

    Refactor userData test data by removing invalid user entries for checkout scenarios, streamlining the dataset for validation purposes.

--- src/utils/test_data/userData.ts
+++ src/utils/test_data/userData.ts
@@ -1,14 +1,8 @@
 export const userData = {
-    validUserForCheckout: {
-      firstName: 'John',
-      lastName: 'Doe',
-      postalCode: '123456'
-    },
-    invalidUserForCheckout: {
-      firstName: 'John@123',
-      lastName: 'Doe@123',
-      postalCode: '123456'
-    }
+  validUserForCheckout: {
+    firstName: 'John',
+    lastName: 'Doe',
+    postalCode: '123456'
+  }
 };
-  
-  
+
